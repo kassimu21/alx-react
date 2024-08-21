3a139752382573bcdf7fd5921fc2c23401fd3f50
kassimu21 <sksallah45@gmail.com>
2024-08-21 11:27:00 +0000

    Bound the actions

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/webpack.config.js
@@ -0,0 +1,10 @@
+// webpack.config.js
+module.exports = {
+  // other configuration options...
+  devServer: {
+    host: '192.168.561', // Listen on all network interfaces
+    port: 8564,      // Specify your port number
+    // other options...
+  },
+};
+
